51e9a2210fb37d732abd792228459f3c611997c7
Fabrice-O-Morin <159199631+Fabrice-O-Morin@users.noreply.github.com>
2025-09-03 21:00:31 +0200

    Initial commit

---
+++ source/training_A4D/training_A4D/tasks/direct/training_a4d/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Training-A4d-Direct-v0",
+    entry_point=f"{__name__}.training_a4d_env:TrainingA4dEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.training_a4d_env_cfg:TrainingA4dEnvCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
